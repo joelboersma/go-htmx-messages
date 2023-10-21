68ddbd6edb95cde0a2ebf75d14aeaf3252f6fccf
Joel Boersma <joelboersma@gmail.com>
2023-10-21 15:12:12 -0700

    ctx/server and query params

--- main.go
+++ main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
+
+const keyServerAddr = "serverAddr"
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Root!\n")
+		hasFirst := r.URL.Query().Has("first")
+		first := r.URL.Query().Get("first")
+		hasSecond := r.URL.Query().Has("second")
+		second := r.URL.Query().Get("second")
+
+		res := fmt.Sprintf("first(%t)=%s, second(%t)=%s\n", hasFirst, first, hasSecond, second)
+
+		io.WriteString(w, res)
 	})
 	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Hello HTTP!\n")
 	})
 
+	ctx := context.Background()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
 	fmt.Println("Running server on port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	err := server.ListenAndServe()
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Println("Server closed")
 	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
-		os.Exit(1)
+		fmt.Printf("Error listening for server: %s\n", err)
 	}
 }
